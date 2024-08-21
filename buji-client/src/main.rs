741527891a9fcb2163974b55d37229b8300116d9
buraksenyurt <burakselimsenyurt@gmail.com>
2024-08-21 21:47:43 +0300

    Implemente a simple client.

---
+++ buji-client/src/main.rs
@@ -0,0 +1,55 @@
+use buji::*;
+use std::thread::sleep;
+use std::time::Duration;
+
+fn main() -> Result<(), String> {
+    let my_game = MyGame::new();
+    let mut buji = GameEngineBuilder::new()?
+        .setup_window()?
+        .change_fps(60)
+        .add_game(Box::new(my_game))
+        .build()?;
+    buji.run()
+}
+
+pub struct Player {
+    pub id: usize,
+    pub nick_name: String,
+    pub health: Option<u8>,
+}
+
+impl Player {
+    pub fn new(id: usize, nick_name: String) -> Player {
+        Self {
+            id,
+            nick_name,
+            health: Some(100),
+        }
+    }
+}
+
+pub struct MyGame {
+    pub players: Vec<Player>,
+    pub level: u8,
+}
+
+impl MyGame {
+    pub fn new() -> Self {
+        Self {
+            players: Vec::new(),
+            level: 0,
+        }
+    }
+}
+
+impl GameObject for MyGame {
+    fn draw(&self) {
+        sleep(Duration::from_secs(5));
+        println!("Draw operations...");
+    }
+
+    fn update(&mut self) -> MainState {
+        println!("Update operations...");
+        MainState::Running
+    }
+}
